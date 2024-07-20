fc8d91860d4d5cb6bdc6119c8fd8f5ff8fe6ee5d
matías federico ciruelos <62398984+ciruelosmf@users.noreply.github.com>
2024-07-20 01:17:48 -0300

    api correc

--- main.py
+++ main.py
@@ -1,32 +1,36 @@
 import os
-import openai
+from openai import OpenAI
+client = OpenAI()
 from aiogram import Bot, Dispatcher, executor, types
 from keep_alive import keep_alive
 
 bot = Bot(token = os.getenv("tg_token"))
 dp = Dispatcher(bot)
 
-openai.api_key = os.getenv("ai_token")
+client.api_key = os.getenv("OPENAI_API_KEY")
 
 keep_alive()
 
 @dp.message_handler(commands = ['start', 'help'])
 async def welcome(message: types.Message):
-  await message.reply('Hello! Im GPT chat bot. Ask me something')
+  await message.reply('Hi. Ask  ')
 
 
 @dp.message_handler()
 async def gpt(message: types.Message):
-  response = openai.Completion.create(
-    model="text-davinci-003",
-    prompt=message.text,
+  response = client.chat.completions.create(
+    model="gpt-4o-mini",
+    messages=[
+        {"role": "system", "content": "You are a helpful assistant."},
+        {"role": "user", "content": f" {message.text}"},
+    ],
     temperature=0.5,
-    max_tokens=1024,
+    max_tokens=424,
     top_p=1,
     frequency_penalty=0.0,
     presence_penalty=0.0
   )
-  await message.reply(response.choices[0].text)
+  await message.reply(response.choices[0].message.content)
 
 
 if __name__ == "__main__":
